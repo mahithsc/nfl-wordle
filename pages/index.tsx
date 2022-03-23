b6fef7596d5a14d53f363cc9873295b8c4d970b8
mahithsc <mahith.chitrapu@gmail.com>
2022-03-23 10:56:26 -0400

    starting with one player

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,15 @@ import styles from '../styles/Home.module.css'
 
 const Home:NextPage = () => {
   return (
-    <>
-      <div>FOODLE</div>
-    </>
+    <div>
+      <div className='bg-green-200 flex justify-center'>
+        <div>NFL Wordle</div>
+      </div>
+      <div className='items-center flex flex-col relative'>
+        <img src="https://a.espncdn.com/i/headshots/nfl/players/full/4241464.png" className='flex-1 brightness-0'/>
+        <div className='absolute h-[100%] w-[100%] bg-white-500 bottom-0 opacity-100'></div>
+      </div>
+    </div>
   )
 }
 
