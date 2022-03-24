3ba2c1116536d65c68773325b81ed913006fb929
mahithsc <mahith.chitrapu@gmail.com>
2022-03-24 10:57:02 -0400

    making putting a container over the picture

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import styles from '../styles/Home.module.css'
 const Home:NextPage = () => {
   return (
     <div>
-      <div className='bg-green-200 flex justify-center'>
-        <div>NFL Wordle</div>
+      <div className='bg-slate-900 flex justify-center'>
+        <div className='text-2xl text-white'>NFL Wordle</div>
       </div>
       <div className='items-center flex flex-col relative'>
         <img src="https://a.espncdn.com/i/headshots/nfl/players/full/4241464.png" className='flex-1 brightness-0'/>
