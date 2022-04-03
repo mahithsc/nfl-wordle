1908a0c9274ac64db9871999a68f3cfee3062c65
mahithsc <mahith.chitrapu@gmail.com>
2022-04-03 18:50:24 -0400

    closinf the filter when needed

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,23 +13,44 @@ const Home: NextPage = () => {
   const players = require('../assets/players_datbase.json')
 
   //keps track of the number of picks the user has made
-  const [numberOfPicks, setNumberOfPicks] = useState(-1)
+  const [numberOfPicks, setNumberOfPicks] = useState(0)
 
   //state variables for the players
   const [choice, setChoice]:any = useState('')
-  const [choiceTwo, setChoiceTwo]:any = useState('')
-  const [choiceThree, setChoiceThree]:any = useState('')
-  const [choiceFour, setChoiceFour]:any = useState('')
-  const [choiceFive, setChoiceFive]:any = useState('')
-  const [choiceSix, setChoiceSix]:any = useState('')
-  const [choiceSeven, setChoiceSeven]:any = useState('')
-  const [choiceEight, setChoiceEight]:any = useState('')
+  const [choiceTwo, setChoiceTwo]:any = useState('hello')
+  const [choiceThree, setChoiceThree]:any = useState('hello')
+  const [choiceFour, setChoiceFour]:any = useState('hello')
+  const [choiceFive, setChoiceFive]:any = useState('hello')
+  const [choiceSix, setChoiceSix]:any = useState('hello')
+  const [choiceSeven, setChoiceSeven]:any = useState('hello')
+  const [choiceEight, setChoiceEight]:any = useState('hello')
+
+  //function which decides which state variable to changfe based on the number of picks
+  const changeStateFunc = (numberOfIterations:number, value:string|null) => {
+    if (numberOfIterations === 1) {
+      setChoice(value)
+    } else if (numberOfIterations === 2) {
+      setChoiceTwo(value)
+    } else if (numberOfIterations === 3) {
+      setChoiceThree(value)
+    } else if (numberOfIterations === 4) {
+      setChoiceFour(value)
+    } else if (numberOfIterations === 5) {
+      setChoiceFive(value)
+    } else if (numberOfIterations === 6) {
+      setChoiceSix(value)
+    } else if (numberOfIterations === 7) {
+      setChoiceSeven(value)
+    } else if (numberOfIterations === 8) {
+      setChoiceEight(value)
+    }
+  }
+
 
 
   useEffect(() => {
-    console.log(choice)
     setNumberOfPicks(picks => picks + 1)
-  }, [choice])
+  }, [choice, choiceTwo, choiceThree, choiceFour, choiceFive, choiceSix, choiceSeven, choiceEight])
 
   useEffect(() => {
     console.log(numberOfPicks)
@@ -44,10 +65,12 @@ const Home: NextPage = () => {
         <img src={players[2].photo} className='flex-1 brightness-[0]' width={500} />
         <div className='absolute h-[100%] w-[100%] bg-white-500 bottom-0'></div>
       </div>
-      <div
-        //make the background color bluw
-        className='flex flex-col items-center mt-11'>
-        <Autocomplete
+      <div className='flex flex-col items-center mt-11'>
+
+        {
+        
+        (numberOfPicks <= 8) ?
+        (<Autocomplete
           className='w-[35rem]'
           disablePortal
           id="free-solo-demo"
@@ -56,17 +79,27 @@ const Home: NextPage = () => {
 
           //when an option is selected, set the state of the choice
           onChange={(event: any, newValue: string | null) => {
-            setChoice(newValue)
+            changeStateFunc(numberOfPicks, newValue)
           }}
 
           sx={{ width: 300 }}
           renderInput={(params) => <TextField {...params} />
           }
-        />
+        />):null
+      }
+
+        
       </div>
 
       <div className='flex flex-col items-center'>
         <div>{JSON.stringify(choice.name)}</div>
+        <div>{JSON.stringify(choiceTwo.name)}</div>
+        <div>{JSON.stringify(choiceThree.name)}</div>
+        <div>{JSON.stringify(choiceFour.name)}</div>
+        <div>{JSON.stringify(choiceFive.name)}</div>
+        <div>{JSON.stringify(choiceSix.name)}</div>
+        <div>{JSON.stringify(choiceSeven.name)}</div>
+        <div>{JSON.stringify(choiceEight.name)}</div>
       </div>
 
 
