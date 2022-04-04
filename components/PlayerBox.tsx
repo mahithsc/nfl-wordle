198fcc1a29212b1c84c5dfa4b231547a4128f642
mahithsc <mahith.chitrapu@gmail.com>
2022-04-03 23:38:42 -0400

    formating the table

--- components/PlayerBox.tsx
+++ components/PlayerBox.tsx
@@ -8,14 +8,13 @@ interface Props {
 const PlayerBox: React.FC<Props> = ({ player, playerOfTheDay }) => {
   return (
     // <div>{player.name}</div>
-    <div className='grid grid-cols-4 border-black border-t-2 bg-slate-200 gap-5'>
-      <div className={`${(player.name === playerOfTheDay.name) ? 'bg-green-600' : null}  justify-center font-bold`}>{player?.name}</div>
+    <div className='grid grid-cols-4 border-black border-t-4 bg-slate-200 w-screen lg:w-[60rem]'>
+      <div className={`${(player.name === playerOfTheDay.name) ? 'bg-green-600' : null} flex justify-center font-bold py-10`}>{player?.name}</div>
       {/* <div>{player?.name}</div> */}
-      <div className={`${(player.playerPosition === playerOfTheDay.playerPosition) ? 'bg-green-600' : null} flex justify-center `}>{player.playerPosition}</div>
-      <div className={`${(player.team === playerOfTheDay.team) ? 'bg-green-600' : null} flex justify-center`}>{player.team}</div>
-      <div className='lg:mx-10 md:mx-5 flex justify-center'>{player.age}</div>
+      <div className={`${(player.playerPosition === playerOfTheDay.playerPosition) ? 'bg-green-600' : null} flex justify-center py-10`}>{player.playerPosition}</div>
+      <div className={`${(player.team === playerOfTheDay.team) ? 'bg-green-600' : null} flex justify-center py-10`}>{player.team}</div>
+      <div className='lg:mx-10 md:mx-5 flex justify-center py-10'>{player.age}</div>
 
-      <div className='h-10'></div>
     </div>
   );
 }
