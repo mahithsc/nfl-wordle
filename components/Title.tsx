b4383661e7114b3d62fccfbd1c9b01f6150968bb
mahithsc <mahith.chitrapu@gmail.com>
2022-04-05 13:38:59 -0400

    wrapping with a tag

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Title = () => {
     return (
         <div className='bg-slate-900 flex justify-center'>
-            <a href = '/'className='text-3xl text-white my-5'>Waddle</a>
+            <Link href = '/'><a className='text-3xl text-white my-5'>Waddle</a></Link>
         </div>
     )
 }
