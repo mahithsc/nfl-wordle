ba817e95d5a5666cae4b6b2deaf18ecde9b40bcc
mahithsc <mahith.chitrapu@gmail.com>
2022-04-05 19:02:02 -0400

    adding credits

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,14 @@ const Home: NextPage = () => {
   return (
     <div className='flex flex-col h-screen w-screen bg-slate-600'>
       <Title/>
-      <div className='flex flex-col h-screen items-center justify-center'>
-        <Link href="/unlimited" ><div className='text-white hover:underline'>Unlimited</div></Link>
-        <Link href="/player-selection"><div className='text-white hover:underline'>Play With Friends</div></Link>
+      <div className='flex flex-col h-screen items-center justify-center space-y-10'>
+        <Link href="/unlimited" ><div className='text-white hover:underline text-2xl'>Unlimited Waddle</div></Link>
+        <Link href="/player-selection"><div className='text-white hover:underline text-2xl'>Play With Friends</div></Link>
+      </div>
+
+      <div className='self-center'>
+        <div className='text-white'>by: <Link href={'https://github.com'}><a className='underline'>Mahith Chitrapu</a></Link></div>
+        <div className='text-white'>Inspired by <Link href={'https://weddlegame.com'}><a className='underline'>Weddle</a></Link></div>
       </div>
     </div>
   )
