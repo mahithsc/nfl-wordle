63ae47578f3b4eeb0ae04bb1b3075f830dbfe900
mahithsc <mahith.chitrapu@gmail.com>
2022-04-06 11:54:49 -0400

    initial commit to this branch

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Title = () => {
     return (
-        <div className='bg-slate-900 flex justify-center'>
+        <div className='bg-slate-900 flex justify-center absolute top-0 w-screen'>
             <Link href = '/'><a className='text-3xl text-white my-5'>Waddle</a></Link>
         </div>
     )
