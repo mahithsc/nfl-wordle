d9e84cbec019adbb87b11fd62c9e47176d6cec78
mahithsc <mahith.chitrapu@gmail.com>
2022-04-06 11:56:23 -0400

    fixing the header

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Title = () => {
     return (
-        <div className='bg-slate-900 flex justify-center absolute top-0 w-screen'>
+        <div className='bg-slate-900 flex justify-center'>
             <Link href = '/'><a className='text-3xl text-white my-5'>Waddle</a></Link>
         </div>
     )
