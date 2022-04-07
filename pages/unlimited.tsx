7888bcd2df4be3a6c93e689bc896b4b1d0865077
mahithsc <mahith.chitrapu@gmail.com>
2022-04-07 18:24:51 -0400

    removing white space

--- pages/unlimited.tsx
+++ pages/unlimited.tsx
@@ -89,7 +89,6 @@ const Unlimited = () => {
   return (
     <div className='min-h-screen bg-[rgb(25,25,25)]'>
       <div className='h-16'></div>
-
       <div>
         <div className='text-center text-white text-5xl'>waddle <div className='text-xl mt-2'>unlimited</div></div>
         {/* <div className='text-center flex-wrap text-white'>you can keep generating new players, and you have 7 tries to guesse each player. let&apos;s see if you can get it!</div> */}
