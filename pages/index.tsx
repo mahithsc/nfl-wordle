2ab27bcffa62e4a4a86b53c0b2f02e0529ba8c85
mahithsc <mahith.chitrapu@gmail.com>
2022-04-13 10:38:56 -0400

    instantly routing to the unlimited page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,31 @@ import type { NextPage } from 'next'
 import React, { useState, useEffect } from 'react';
 import Title from '../components/Title';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    //route to /unlimited
+    router.push('/unlimited');
+  })
   return (
-    <div className='flex flex-col h-screen w-screen bg-black'>
-      <Title />
-      <div className='flex flex-col h-screen items-center justify-center space-y-10'>
-        <Link href={'/unlimited'}>
-          <a className='text-white text-3xl hover:underline'>Waddle</a>
-        </Link>
+    <div></div>
+    
+    // <div className='flex flex-col h-screen w-screen bg-black'>
+    //   <Title />
+    //   <div className='flex flex-col h-screen items-center justify-center space-y-10'>
+    //     <Link href={'/unlimited'}>
+    //       <a className='text-white text-3xl hover:underline'>Waddle</a>
+    //     </Link>
 
-      </div>
+    //   </div>
 
-      <div className='self-center mb-20'>
-        <div className='text-white'>by: <Link href={'https://www.linkedin.com/in/mchitrapu/'}><a className='underline'>Mahith Chitrapu</a></Link></div>
-        <div className='text-white'>Inspired by <Link href={'https://weddlegame.com'}><a className='underline'>Weddle</a></Link></div>
-      </div>
-    </div>
+    //   <div className='self-center mb-20'>
+    //     <div className='text-white'>by: <Link href={'https://www.linkedin.com/in/mchitrapu/'}><a className='underline'>Mahith Chitrapu</a></Link></div>
+    //     <div className='text-white'>Inspired by <Link href={'https://weddlegame.com'}><a className='underline'>Weddle</a></Link></div>
+    //   </div>
+    // </div>
   )
 }
 
