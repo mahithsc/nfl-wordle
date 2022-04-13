91f223450b4dff39d416eec2fdc28a381e12ebb5
mahithsc <mahith.chitrapu@gmail.com>
2022-04-13 10:25:05 -0400

    adding credits at the bottom

--- pages/how-to-play.tsx
+++ pages/how-to-play.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import Title from '../components/Title'
 
@@ -29,7 +30,7 @@ const HowToPlay = () => {
           </div> */}
 
           <div className='bg-black p-5 rounded-xl mt-5'>
-            <div className='font-bold text-2xl'>rules for waddle unlimited</div>
+            <div className='font-bold text-2xl'>how to play waddle</div>
             <div className='mt-5'>
               <li className='mb-2'>you have 7 guesses to get the right player</li>
               <li className='mb-2'>use the search bar to find the player that you're thinking off</li>
@@ -40,22 +41,23 @@ const HowToPlay = () => {
           </div>
 
           <div className='bg-black p-5 rounded-xl mt-5'>
-            <div className='font-bold text-2xl'>rules for waddle vs. friends</div>
+            <div className='font-bold text-2xl'>how to play waddle vs. friends</div>
             <div className='mt-5'>
-              <li className='mb-2'>you have 7 guesses to get the right player</li>
-              <li className='mb-2'>use the search bar to find the player that you're thinking off</li>
-              <li className='mb-2'> <span className='bg-green-500 p-1 text-black'>green</span> square means that that metric is exactly correct</li>
-              <li> <span className='bg-yellow-500 p-1 text-black'>yellow</span> square means that that metric is close but not exactly ex. if you chose a player in the right confrence but not correct division the square will be yellow</li>
-              <li className='mb-2'>once your 7 guesses are up or you get the correct player, click generate new player to play again!</li>
+              <li className='mb-2'>use the search bar to select the secret player you wish to share with your friend</li>
+              <li className='mb-2'>once you have slected the player of your liking, click the copy link button</li>
+              <li>this button generates and copies to you device a unique link you can share with you friend</li>
             </div>
           </div>
-
-
-
         </div>
-
-
       </div>
+
+      <div className='mt-7'>
+      <div className='text-white text-center'>created by: <Link href={'https://www.linkedin.com/in/mchitrapu/'}><a className='underline'>Mahith Chitrapu</a></Link></div>
+      <div className='text-white text-center'>inspired by the original: <Link href={'https://weddlegame.com'}><a className='underline'>Weddle</a></Link></div>
+      </div>
+
+      
+
     </div>
   )
 }
