3bc5a92a552f57fb89496e42c10d848c2e6ed04e
mahithsc <mahith.chitrapu@gmail.com>
2022-04-13 09:54:35 -0400

    formatting instructions

--- pages/how-to-play.tsx
+++ pages/how-to-play.tsx
@@ -4,15 +4,31 @@ import Title from '../components/Title'
 const HowToPlay = () => {
   return (
     <div className='min-h-screen bg-[rgb(25,25,25)]'>
-      <Title/>
+      <Title />
       <div className='text-white'>
         <div className='h-16'></div>
-      <div>
-        <div className='text-center text-white text-5xl'>waddle <div className='text-xl mt-2'>how to play</div></div>
-        {/* <div className='text-center flex-wrap text-white'>you can keep generating new players, and you have 7 tries to guesse each player. let&apos;s see if you can get it!</div> */}
-      </div>
+        <div>
+          <div className='text-center text-white text-5xl'>waddle <div className='text-xl mt-2'>how to play</div></div>
+          {/* <div className='text-center flex-wrap text-white'>you can keep generating new players, and you have 7 tries to guesse each player. let&apos;s see if you can get it!</div> */}
+        </div>
 
-      <div className='h-16'></div>
+        <div className='h-12'></div>
+
+        <div className='text-white ml-10 mr-10'>
+          <div className='bg-black p-5 rounded-xl'>
+            <div className='font-bold text-2xl'>objective of the game</div>
+            <div className='mt-5'>the main idea of the game is to guesse the secret player in as little tries as possible. you have 7 tries to guesse each player. its that simple!</div>
+          </div>
+
+          <div className='bg-black p-5 rounded-xl mt-5'>
+          <div className='font-bold text-2xl'>so how do I guess</div>
+          <div className='mt-5'>the first time </div>
+          </div>
+
+
+         
+        </div>
+
 
       </div>
     </div>
