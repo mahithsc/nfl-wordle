64b8195b634a8a4a0884da1d1775490ca3e54a7a
mahithsc <mahith.chitrapu@gmail.com>
2022-04-16 19:06:48 -0400

    commenting for the header bar

--- components/HeaderForPlayerBox.tsx
+++ components/HeaderForPlayerBox.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 const HeaderForPlayerBox = () => {
     return (
         <>
+
+        {/* this is the block for smaller screens */}
             <div className='grid grid-cols-4 w-screen lg:w-[50rem]'>
                 <div className='flex justify-center text-white'>position</div>
                 <div className='flex justify-center text-white'>team</div>
                 <div className='flex justify-center text-white'>age</div>
                 <div className='flex justify-center text-white'>division</div>
             </div>
+
+            {/* this is the for larger screebs */}
             {/* <div className='grid grid-cols-5 w-screen lg:w-[50rem]'>
                 <div className='flex justify-center text-white'>draft class</div>
                 <div className='flex justify-center text-white'>position</div>
