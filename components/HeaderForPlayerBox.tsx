3eead3b96e45409e12a183db8eb53098acad9b65
mahithsc <mahith.chitrapu@gmail.com>
2022-04-16 19:11:41 -0400

    finished the ui when the screen size changes

--- components/HeaderForPlayerBox.tsx
+++ components/HeaderForPlayerBox.tsx
@@ -4,21 +4,25 @@ const HeaderForPlayerBox = () => {
     return (
         <>
 
-        {/* this is the block for smaller screens */}
-            <div className='grid grid-cols-4 w-screen lg:w-[50rem]'>
-                <div className='flex justify-center text-white'>position</div>
-                <div className='flex justify-center text-white'>team</div>
-                <div className='flex justify-center text-white'>age</div>
-                <div className='flex justify-center text-white'>division</div>
+            {/* this is the block for smaller screens */}
+            <div className='md:hidden'>
+                <div className='grid grid-cols-4 w-screen lg:w-[50rem]'>
+                    <div className='flex justify-center text-white'>position</div>
+                    <div className='flex justify-center text-white'>team</div>
+                    <div className='flex justify-center text-white'>age</div>
+                    <div className='flex justify-center text-white'>division</div>
+                </div>
             </div>
 
-            {/* this is the for larger screebs */}
-            <div className='grid grid-cols-5 w-screen lg:w-[50rem]'>
+            {/* this is the for larger screens */}
+            <div className='hidden md:block'>
+            <div className=' grid grid-cols-5 w-screen lg:w-[50rem]'>
                 <div className='flex justify-center text-white'>name</div>
                 <div className='flex justify-center text-white'>position</div>
                 <div className='flex justify-center text-white'>team</div>
                 <div className='flex justify-center text-white'>age</div>
                 <div className='flex justify-center text-white'>division</div>
+            </div>
             </div>
         </>
         // <div>{player.name}</div>
--- components/PlayerBox.js
+++ components/PlayerBox.js
@@ -61,9 +61,9 @@ const PlayerBox = ({ player, playerOfTheDay, changeStateTrue }) => {
 
       {/* block for large screens */}
       <div className='border-t-8 border-[rgb(25,25,25)] hidden md:block'>
-        <div className={`${(player.name === playerOfTheDay.name) ? 'bg-green-600' : null} flex border-black bg-black  text-white font-bold pt-2 pl-2 pb-2`}>{player?.name}</div>
+        {/* <div className={`${(player.name === playerOfTheDay.name) ? 'bg-green-600' : null} flex border-black bg-black  text-white font-bold pt-2 pl-2 pb-2`}>{player?.name}</div> */}
         <div className='grid grid-cols-5  bg-black w-screen lg:w-[50rem] border-t-[rgb(204, 204, 204)] border-t-2'>
-          <div className={`${(player.playerPosition === playerOfTheDay.playerPosition) ? 'bg-green-600' : null} flex justify-center py-10 text-white`}>{player.playerPosition}</div>
+          <div className={`${(player.playerPosition === playerOfTheDay.playerPosition) ? 'bg-green-600' : null} flex justify-center py-10 text-white`}>{player.name}</div>
           <div className={`${(player.playerPosition === playerOfTheDay.playerPosition) ? 'bg-green-600' : null} flex justify-center py-10 text-white`}>{player.playerPosition}</div>
           <div className={`${(player.team === playerOfTheDay.team) ? 'bg-green-600' : null} flex justify-center py-10 text-white`}>{player.team}</div>
           <div className={`${(age === playerOfTheDayAge) ? 'bg-green-600' : null} flex justify-center py-10 text-white`}>{`${age}`}
